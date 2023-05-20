4339691a790d8a5700048d266c1a39ac86e22eb5
Job Henandez Lara <hj93@protonmail.com>
2023-05-19 23:47:08 -0700

    Update server.lisp

--- src/server.lisp
+++ src/server.lisp
@@ -12,3 +12,14 @@
 	 (expr (compile-scheme e3)))
     (setf (gethash 'expression ht) (write-to-string expr))
     (stringify ht :pretty t :stream t)))
+
+
+(define-easy-handler (cps* :uri "/cps") (exp)
+  (setf (content-type*) "application/json")
+  (let* ((cpsht (make-hash-table :test 'equal))
+	 (e (list (read-from-string exp)))
+	 (e2 `(,e))
+	 (e3 (car (car e2)))
+	 (expr (cps (desugar (parse-exp e3)) 'halt)))
+    (setf (gethash 'expression cpsht) (write-to-string expr))
+    (stringify cpsht :pretty t :stream t)))
