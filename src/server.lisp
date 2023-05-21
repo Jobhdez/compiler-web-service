fba9331b7fa4b98778806331797921ef3c994b97
Izaak Walton <izaakw@protonmail.com>
2023-05-20 19:53:29 -0700

    Added (launch) for app, dependency info, and README edit.

--- src/server.lisp
+++ src/server.lisp
@@ -1,7 +1,40 @@
 (in-package #:lambda-server)
 
+;(defun start-server ()
+ ; (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4243)))
+
+(defvar *server*
+  (make-instance 'hunchentoot:easy-acceptor
+                 :port 4243))
+
 (defun start-server ()
-  (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4243)))
+  (hunchentoot:start *server*))
+
+(defun stop-server ()
+  (hunchentoot:stop *server*))
+                 ;:document-root (asdf:system-relative-pathname "compiler-web-service" "webbartleby/www/")))
+
+;;; Launching in the browser:
+
+;;; adopted from https://github.com/eudoxia0/trivial-open-browser, thank you
+(defparameter +format-string+
+  #+(or win32 mswindows windows)
+  "explorer ~S"
+  #+(or macos darwin)
+  "open ~S"
+  #-(or win32 mswindows macos darwin windows)
+  "xdg-open ~S")
+
+(defun open-browser (url)
+  "Opens the browser to your target url"
+  (uiop:run-program (format nil +format-string+ url)))
+
+(defun launch ()
+  (start-server)
+  (uiop:run-program
+   (format nil
+           +format-string+
+           "http://localhost:4243/compile?exp=(if (= 2 2) 2 3)")))
 
 (defmacro define-route (url view-fn compile-fn ht)
   `(define-easy-handler (,view-fn :uri ,url) (exp)
