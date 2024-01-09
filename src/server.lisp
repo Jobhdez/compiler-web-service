83a541e4922cd899806abf666bfa4226c03f46ff
Job Hernandez Lara <hj93@protonmail.com>
2024-01-09 13:40:57 -0800

    add listing

--- src/server.lisp
+++ src/server.lisp
@@ -1,7 +1,7 @@
 (in-package #:lambda-server)
 
-;(defun start-server ()
- ; (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4243)))
+					;(defun start-server ()
+					; (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4243)))
 
 (defvar *server*
   (make-instance 'hunchentoot:easy-acceptor
@@ -12,7 +12,7 @@
 
 (defun stop-server ()
   (hunchentoot:stop *server*))
-                 ;:document-root (asdf:system-relative-pathname "compiler-web-service" "webbartleby/www/")))
+					;:document-root (asdf:system-relative-pathname "compiler-web-service" "webbartleby/www/")))
 
 ;;; Launching in the browser:
 
@@ -36,19 +36,19 @@
            +format-string+
            "http://localhost:4243/compile?exp=(if (= 2 2) 2 3)")))
 (setf user-map 
-   (make-hash-table
-:test #'equal
-:size 10000
-:rehash-size 10000
-:rehash-threshold 0.8
-:weakness nil
-:synchronized nil))
+      (make-hash-table
+       :test #'equal
+       :size 10000
+       :rehash-size 10000
+       :rehash-threshold 0.8
+       :weakness nil
+       :synchronized nil))
 
 (setf auth-map (make-hash-table :test #'equal))
 
 (define-easy-handler (hello :uri "/hello") ()
   (format nil 
-"<h4>hello</h4>"))
+	  "<h4>hello</h4>"))
 
 (define-easy-handler (scm :uri "/scm") ()
   (setf (content-type*) "application/json")
@@ -79,11 +79,11 @@
       (create-dao 'scm-lambda-exp :exp exp :user (find-dao 'user :username username) :eval-exp (write-to-string er))
       (setf (gethash 'response table) "Expressions Created!")
       (stringify table))))
-     
-	 
+
+
 
 (define-easy-handler (scmlamtwo :uri "/scmlamtwo") ()
-   (setf (content-type*) "application/json")
+  (setf (content-type*) "application/json")
   (setf (header-out "Access-Control-Allow-Origin") *)
   (setf *show-lisp-errors-p* t)
 
@@ -97,14 +97,19 @@
 	 (table2 (make-hash-table :test 'equal)))
     (if (gethash username auth-map)
 	(progn
-           (create-dao 'scm-lambda-exp :exp exp :user (find-dao 'user :username username) :eval-exp (write-to-string er))
-           (setf (gethash 'response table) "Expressions Created!")
-           (stringify table))
+          (create-dao 'scm-lambda-exp :exp exp :user (find-dao 'user :username username) :eval-exp (write-to-string er))
+          (setf (gethash 'response table) "Expressions Created!")
+          (stringify table))
 	(progn
 	  (setf (gethash 'response table2) "Must Be Logged in")
 	  (stringify table2)))))
-	      
-	    
+
+(define-easy-handler (scm-exps :uri "/scm-exps") ()
+  (setf (content-type*) "application/json")
+  (setf (header-out "Access-Control-Allow-Origin") "*")
+  (setf *show-lisp-errors-p* t)
+  (stringify (select-dao 'scm-lambda-exp)))
+
 
 (defmacro define-route (url view-fn compile-fn ht)
   `(define-easy-handler (,view-fn :uri ,url) (exp)
@@ -122,10 +127,10 @@
 
 ;;index------
 (hunchentoot:define-easy-handler (index :uri "/") (info)
-    (setf (hunchentoot:content-type*) "text/html")
-      (let ((the-user (hunchentoot:session-value :user)))
-         (format nil 
-"<h4>welcome-to-the-small-demo</h4><hr>
+  (setf (hunchentoot:content-type*) "text/html")
+  (let ((the-user (hunchentoot:session-value :user)))
+    (format nil 
+	    "<h4>welcome-to-the-small-demo</h4><hr>
 <p>info:~A</p>
 <p>user:~A</p>
 <a href=\"/sign-up\">click-here-to-sign-up</a><br>
@@ -134,9 +139,9 @@
 
 ;;sign-up------
 (hunchentoot:define-easy-handler (sign-up :uri "/sign-up") (info)
-   (setf (hunchentoot:content-type*) "text/html")
-      (format nil 
-"<h4>welcome-to-sign-up</h4><hr>
+  (setf (hunchentoot:content-type*) "text/html")
+  (format nil 
+	  "<h4>welcome-to-sign-up</h4><hr>
 <p>info:~A</p>
 <a href=\"/\">click-here-to-index</a><br>
 <form action=\"/sign-up-ok\" method=\"post\">
@@ -147,28 +152,28 @@ email:<br><input type=\"text\" name=\"email\" /><br>
 <input type=\"submit\" value=\"submit\" /></form>" info))
 
 (hunchentoot:define-easy-handler (sign-up-ok :uri "/sign-up-ok") ()
-   (setf (hunchentoot:content-type*) "text/plain")
-      (let ((the-user   (hunchentoot:post-parameter "user"))
-            (the-pass   (hunchentoot:post-parameter "pass"))
-	    (the-name   (post-parameter "name"))
-	    (the-email (post-parameter "email")))
-         (if   (and  (stringp the-user) (stringp the-pass)
-                     (< 1 (length the-user) 200) (< 1 (length the-pass) 200))
-               (if   (gethash the-user user-map)
-                     (hunchentoot:redirect "/sign-up?info=has-been-used")
-                     (progn
-                       (setf (gethash the-user user-map) (cl-pass:hash the-pass))
-		       (create-dao 'user :name the-name :username the-user :email the-email)
-                       (hunchentoot:redirect "/sign-in")))                 
-               (hunchentoot:redirect "/sign-up?info=input-error"))))
+  (setf (hunchentoot:content-type*) "text/plain")
+  (let ((the-user   (hunchentoot:post-parameter "user"))
+        (the-pass   (hunchentoot:post-parameter "pass"))
+	(the-name   (post-parameter "name"))
+	(the-email (post-parameter "email")))
+    (if   (and  (stringp the-user) (stringp the-pass)
+                (< 1 (length the-user) 200) (< 1 (length the-pass) 200))
+          (if   (gethash the-user user-map)
+                (hunchentoot:redirect "/sign-up?info=has-been-used")
+                (progn
+                  (setf (gethash the-user user-map) (cl-pass:hash the-pass))
+		  (create-dao 'user :name the-name :username the-user :email the-email)
+                  (hunchentoot:redirect "/sign-in")))                 
+          (hunchentoot:redirect "/sign-up?info=input-error"))))
 
 ;;sign-in------
 (hunchentoot:define-easy-handler (sign-in :uri "/sign-in") (info)
-   (setf (hunchentoot:content-type*) "text/html")
-      (if   (hunchentoot:session-value :user)
-            (hunchentoot:redirect "/?info=you-had-sign-in")
-            (format nil 
-"<h4>welcome-to-sign-in</h4><hr>
+  (setf (hunchentoot:content-type*) "text/html")
+  (if   (hunchentoot:session-value :user)
+        (hunchentoot:redirect "/?info=you-had-sign-in")
+        (format nil 
+		"<h4>welcome-to-sign-in</h4><hr>
 <p>info:~A</p>
 <a href=\"/\">click-here-to-index</a><br>
 <form action=\"/sign-in-ok\" method=\"post\">
@@ -177,19 +182,19 @@ pass:<br><input type=\"password\" name=\"pass\" /><br>
 <input type=\"submit\" value=\"submit\" /></form>" info)))
 
 (hunchentoot:define-easy-handler (sign-in-ok :uri "/sign-in-ok") ()
-   (setf (hunchentoot:content-type*) "text/plain")
-      (let ((the-user   (hunchentoot:post-parameter "user"))
-            (the-pass   (hunchentoot:post-parameter "pass")))
-         (if   (and (stringp the-user) (stringp the-pass))
-               (let ((pass-info (gethash the-user user-map)))
-                  (if   (ignore-errors (cl-pass:check-password the-pass pass-info))
-                        (progn
-                           (hunchentoot:start-session)
-                           (setf (hunchentoot:session-value :user) the-user)
-			   (setf (gethash the-user auth-map) the-user)
-                           (hunchentoot:redirect "/?info=you-have-sign-in"))
-                        (hunchentoot:redirect "/sign-in?info=password-error")))
-               (hunchentoot:redirect "/sign-in?info=input-error"))))
+  (setf (hunchentoot:content-type*) "text/plain")
+  (let ((the-user   (hunchentoot:post-parameter "user"))
+        (the-pass   (hunchentoot:post-parameter "pass")))
+    (if   (and (stringp the-user) (stringp the-pass))
+          (let ((pass-info (gethash the-user user-map)))
+            (if   (ignore-errors (cl-pass:check-password the-pass pass-info))
+                  (progn
+                    (hunchentoot:start-session)
+                    (setf (hunchentoot:session-value :user) the-user)
+		    (setf (gethash the-user auth-map) the-user)
+                    (hunchentoot:redirect "/?info=you-have-sign-in"))
+                  (hunchentoot:redirect "/sign-in?info=password-error")))
+          (hunchentoot:redirect "/sign-in?info=input-error"))))
 
 ;;sign-out------
 (hunchentoot:define-easy-handler (sign-out :uri "/sign-out") ()
@@ -226,7 +231,7 @@ pass:<br><input type=\"password\" name=\"pass\" /><br>
   ((exp :col-type :text)
    (user :col-type user)
    (eval-exp :col-type :text)))
-  
+
 
 (deftable cps-exp ()
   ((exp :col-type :text)
