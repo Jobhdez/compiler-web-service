d6c8ffebdc0c2f2df59868e37635de3f481f6f0d
Job Hernandez Lara <hj93@protonmail.com>
2024-01-10 16:56:38 -0800

    add zetta compiler

--- src/compilation-functions.lisp
+++ src/compilation-functions.lisp
@@ -4,3 +4,13 @@
 
 (defun compile-cps (e)
   (cps (desugar (parse-exp e)) 'halt))
+
+(defun identity* (e)
+  e)
+
+(defun read-str (e)
+  (read-from-string e))
+
+(defun compile-zetta (e)
+  (let ((ast (parse-with-lexer (token-generator (lex-line e)) *python-grammar*)))
+    (select-instructions (remove-complex-operands ast))))
