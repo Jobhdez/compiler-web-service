cba2eefc4feaac1894939514d99247b5465011d0
Job Hernandez Lara <hj93@protonmail.com>
2024-01-10 16:57:35 -0800

    remove a package

--- src/package.lisp
+++ src/package.lisp
@@ -16,9 +16,9 @@
   (:export #:select-instructions
 	   #:remove-complex-operands
 	   #:assign-homes
-	   #:zetta-parse-with-lexer
-	   #:zetta-token-generator
-	   #:zetta-lex-line
+	   #:parse-with-lexer
+	   #:token-generator
+	   #:lex-line
 	   #:*python-grammar*
 	   #:clean
 
@@ -45,88 +45,12 @@
 (defpackage #:zetta-var
   (:use))
 
-(defpackage #:yotta
-  (:use #:common-lisp
-	#:alexa
-	#:yacc
-	#:alexandria
-	#:trivia)
-  (:export #:yotta-token-generator
-	   #:make-lisp-interlan
-	   #:yotta-lex-line
-	   #:*linear-algebra-grammar*
-	   #:yotta-parse-with-lexer
-	   #:program
-	   #:make-program
-	   #:plus
-	   #:make-plus
-	   #:minus
-	   #:make-minus
-	   #:mul
-	   #:make-mul
-	   #:vec
-	   #:make-vec
-	   #:matrix
-	   #:make-matrix
-	   #:num
-	   #:make-num
-	   #:var
-	   #:make-var
-	   #:assignment
-	   #:make-assignment
-	   #:defvarlisp
-	   #:make-defvarlisp
-	   #:vectorlisp
-	   #:make-vectorlisp
-	   #:matrixlisp
-	   #:make-matrixlisp
-	   #:numlisp
-	   #:make-numlisp
-	   #:looplisp
-	   #:make-looplisp
-	   #:vectorsum
-	   #:make-vectorsum
-	   #:vectorminus
-	   #:make-vectorminus
-	   #:matrixsum
-	   #:make-matrixsum
-	   #:matrixminus
-	   #:make-matrixminus
-	   #:setflisp
-	   #:make-setflisp
-	   #:setqlisp
-	   #:make-setqlisp
-	   #:areflisp
-	   #:make-areflisp
-	   #:sumlisp
-	   #:make-sumlisp
-	   #:minuslisp
-	   #:make-minuslisp
-	   #:prognlisp
-	   #:make-prognlisp
-	   #:letexpressionlisp
-	   #:make-letexpression
-	   #:dotproduct
-	   #:make-dotproduct
-	   #:fakevecmul
-	   #:make-fakevecmul
-	   #:matrixmul
-	   #:make-matrixmul
-	   #:incflisp
-	   #:make-incflisp
-	   #:mullisp
-	   #:make-mullisp))
-
-(defpackage #:yotta-var
-  (:use))
-
 (defpackage #:lambda-server
   (:use #:common-lisp
 	#:scheme-to-lambda-calculus
 	#:hunchentoot
 	#:cps-compiler
 	#:zetta
-	#:yotta
 	#:com.inuoe.jzon
 	#:mito)
   (:export #:start-server
