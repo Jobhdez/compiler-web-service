f9b860193b141c7d96f7c36ff4cbb29c25866e4e
Job Hernandez <hj93@protonmail.com>
2024-04-16 00:23:10 -0700

    structure the site better

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Services from "./Services";
+import Button from "@mui/material/Button";
 
 function Home() {
   return (
     <div
       style={{
         minHeight: "100vh", // Ensure the content stretches to at least the full viewport height
-        background: "linear-gradient(to bottom, #006400, #FFFFFF)",
+        background: "linear-gradient(to bottom, #3CB371, #FFFFFF)",
         paddingTop: "100px",
         paddingBottom: "100px",
         position: "relative", // Add relative positioning to the container
@@ -16,36 +17,30 @@ function Home() {
     >
       <Grid container spacing={2} alignItems="center" justifyContent="center">
         <Grid item xs={6}>
-          <Typography variant="h1" align="center">
-            {" "}
-            {/* Centered typography */}
-            Efficient
+          <Typography variant="h3" align="center" fontFamily="monospace">
+            Effient
           </Typography>
-          <Typography variant="h2" align="center">
-            {" "}
-            {/* Centered typography */}
-            Computation for the cloud
+          <Typography variant="h3" align="center" fontFamily="monospace">
+            compilation engine
           </Typography>
         </Grid>
         <Grid item xs={6}>
-          <Typography variant="h3">
-            {" "}
-            {/* Typography without alignment */}
-            An efficient Scheme desugarer
+          <Typography variant="h6" fontFamily="monospace">
+            Scheme to CPS compilation
           </Typography>
-          <Typography variant="h3">
-            {" "}
-            {/* Typography without alignment */}A powerful linear algebra
-            compiler
+          <Typography></Typography>
+          <Typography variant="h6" fontFamily="monospace">
+            Generate efficient linear algebra kernels
           </Typography>
-          <Typography variant="h3">
-            {" "}
-            {/* Typography without alignment */}A fast CPS converter and Python
-            like language
+          <Typography></Typography>
+          <Typography variant="h6" fontFamily="monospace">
+            API for a Python like compiler
           </Typography>
         </Grid>
       </Grid>
-      <Services />
+      <div style={{ marginTop: "350px", marginLeft: "20px" }}>
+        <Services />
+      </div>
     </div>
   );
 }
