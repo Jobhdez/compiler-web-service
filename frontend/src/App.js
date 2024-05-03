e2e7a03770807909b2ec9be4b21142fea41a4fa8
Job Hernandez <hj93@protonmail.com>
2024-05-02 17:11:56 -0700

    add new pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CServe from "./components/CServe";
+import About from "./components/About";
+import OpenSourceProducts from "./components/OpenSourceProducts";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/cserve" element={<CServe />} />
+        <Route path="/open source products" element={<OpenSourceProducts />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </Router>
