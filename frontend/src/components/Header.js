a8c36e68f7892c5caf22aa664176561b781b4db4
Job Hernandez <hj93@protonmail.com>
2024-05-02 17:12:26 -0700

    add page links

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,71 +3,67 @@ import { styled } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import MenuIcon from "@mui/icons-material/Menu";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
 import { NavLink as MuiLink } from "react-router-dom";
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
-  alignItems: "flex-start",
-  paddingTop: theme.spacing(1),
-  paddingBottom: theme.spacing(2),
-  // Override media queries injected by theme.mixins.toolbar
-  "@media all": {
-    minHeight: 1,
-  },
-}));
-
-const StyledImg = styled("img")(({ theme }) => ({
-  marginRight: theme.spacing(30),
-  maxHeight: 150,
+  display: "flex",
+  justifyContent: "space-between",
 }));
 
 function Header() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" style={{ background: "#3CB371" }}>
-        <StyledToolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="black"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-            onClick={handleClick}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{ "aria-labelledby": "basic-button" }}
-          >
-            <MenuItem>Listing</MenuItem>
-            <MenuItem>Detailed</MenuItem>
-          </Menu>
-          <Typography
-            variant="h5"
-            noWrap
-            component="div"
-            color="black"
+        <Toolbar>
+          <MuiLink
+            to="/"
+            color="inherit"
             fontFamily="monospace"
-            sx={{ flexGrow: 1, alignSelf: "flex-end" }}
-          ></Typography>
-        </StyledToolbar>
+            align="center"
+            style={{ textDecoration: "none", marginRight: "20px" }}
+          >
+            <Typography variant="h6" color="white">
+              Manifold ML
+            </Typography>
+          </MuiLink>
+
+          <MuiLink
+            to="/cserve"
+            color="white"
+            fontFamily="monospace"
+            align="center"
+            style={{ textDecoration: "none", marginRight: "20px" }}
+          >
+            <Typography variant="h6" color="white" align="center">
+              CServe
+            </Typography>
+          </MuiLink>
+
+          <MuiLink
+            to="/open-source-products"
+            color="white"
+            fontFamily="monospace"
+            align="center"
+            style={{ textDecoration: "none", marginRight: "20px" }}
+          >
+            <Typography variant="h6" color="white">
+              Open Source Products
+            </Typography>
+          </MuiLink>
+
+          <MuiLink
+            to="/about"
+            color="inherit"
+            fontFamily="monospace"
+            align="center"
+            style={{ textDecoration: "none" }}
+          >
+            <Typography variant="h6" color="white">
+              About
+            </Typography>
+          </MuiLink>
+        </Toolbar>
       </AppBar>
     </Box>
   );
